121ecd7d97e45ead7bc57cdc8182cd904556f5cf
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-02-01 14:12:30 +0200

    Update quadratic_equations.py

--- quadratic_equations.py
+++ quadratic_equations.py
@@ -11,7 +11,13 @@ t=0
 a=0
 b=0
 c=0
-def picture():
+def pictureW():
+    x1 = np.arange(0,9.5,0.5)
+    y1 = (2/27)*x1*x1-3
+    x2 = np.arange(-10,0.5,0.5)
+    y2 = 0.04*x2*x2-3
+    x3 = np.arange(-9,-2.5,0.5)
+    y3 = (2/9)*(x3+6)**2+1
     x4 = np.arange(-3,9.5,0.5)
     y4 = (-1/12)*(x4-3)**2+6
     x5= np.arange(5,9,0.5)
@@ -29,6 +35,71 @@ def picture():
     fig = plt.figure()
     plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
     plt.title("Whale")
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+
+def pictureG():
+    x1 = np.arange(-9,-0.5,0.5)
+    y1 = -1*(1/16)*(x1+5)**2+2
+    x2 = np.arange(1,9.5,0.5)
+    y2 = -1*(1/16)*(x2-5)**2+2
+    x3 = np.arange(-9,-0.5,0.5)
+    y3 = (1/4)*(x3+5)**2-3
+    x4 = np.arange(1,9.5,0.5)
+    y4 = (1/4)*(x4-5)**2-3
+    x5= np.arange(-9,-5.5,0.5)
+    y5 = -1*(x5+7)**2+5
+    x6 = np.arange(6,9.5,0.5)
+    y6 = -1*(x6-7)**2+5
+    x7 = np.arange(-1,1.5,0.5)
+    y7 = -1*0.5*(x7*x7)+1.5
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title("Glasses")
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+def pictureF():
+    x1 = np.arange(-7,7.5,0.5)
+    y1 = -1*(3/49)*(x1*x1)+1
+    x2 = np.arange(-7,7.5,0.5)
+    y2 = (4/49)*(x2*x2)+1
+    x3 = np.arange(-6.8,-1.5,0.5)
+    y3 = -0.75*(x3+4)**2+11
+    x4 = np.arange(2,7.3,0.5)
+    y4 = -0.75*(x4-4)**2+11
+    x5 = np.arange(-5.8,-2.3,0.5)
+    y5 = -1*(x5+4)**2+9
+    x6 = np.arange(2.8,6.3,0.5)
+    y6 = -1*(x6-4)**2+9
+    x7 = np.arange(-4,4.5,0.5)
+    y7 = (4/9)*(x7*x7)-5
+    x8 = np.arange(-5.2,5.7,0.5)
+    y8 = (4/9)*(x8*x8)-9
+    x9 = np.arange(-7,2.3,0.5)
+    y9 = -1*(1/16)*(x9+3)**2-6
+    x10 = np.arange(2.8,7.5,0.5)
+    y10 = -1*(1/16)*(x10-3)**2-6
+    x11 = np.arange(-7,0.5,0.5)
+    y11 = (1/9)*(x11+4)**2-11
+    x12 = np.arange(0,7.5,0.5)
+    y12 = (1/9)*(x12-4)**2-11
+    x13 = np.arange(4.5,7.7,0.5)
+    y13 = -1*(x13-5)**2
+    x14 = np.arange(-7.-4,0.5)
+    y14 = -1*(x13+5)**2
+    x15 = np.arange(-3,3.5,0.5)
+    y15 = (2/9)*(x15*x15)+2
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14,x15,y15)
+    plt.title("Glasses")
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
 
 def graphics_table():
     flag,D,t=Answer()
@@ -85,6 +156,7 @@ def yet():
     else:
         window.geometry(str(window.winfo_width())+"x"+str(window.winfo_height()-200))
         button_2.config(text="Enlarge window")
+        t=0
 
 window = Tk()
 window.title("quadratic equations")
@@ -108,9 +180,9 @@ button_1=Button(f1,text="Answer",height=1,width=10,font="Arial 15",fg="#468499",
 button_2=Button(f2,text="Enlarge window",height=1,width=10,font="Arial 15",fg="#468499",bg="#DDDDDD", command=yet)
 
 var = IntVar()
-r1 = Radiobutton(f2,text="Whale",variable=var,var=1,font="Arial 15")
-r2 = Radiobutton(f2,text="Glasses",variable=var,var=1,font="Arial 15")
-r3 = Radiobutton(f2,text="Frog",variable=var,var=1,font="Arial 15")
+r1 = Radiobutton(f2,text="Whale",variable=var,var=1,font="Arial 15",command=pictureW)
+r2 = Radiobutton(f2,text="Glasses",variable=var,var=1,font="Arial 15",command=pictureG)
+r3 = Radiobutton(f2,text="Frog",variable=var,var=1,font="Arial 15",command=pictureF)
 
 lbl_1.pack()
 lbl_5.pack()
@@ -126,6 +198,7 @@ r1.pack()
 r2.pack()
 r3.pack()
 button_1.bind("<Button-1>", Answer)
+
 
 
 
